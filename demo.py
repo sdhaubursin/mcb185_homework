aa8dccf1b20a238d503c8e19147dfc5d7e4cff02
sydney haubursin <sdhaubursin@ucdavis.edu>
2026-02-12 15:50:38 -0800

    practice

--- demo.py
+++ demo.py
@@ -1,3 +1,21 @@
-print ("hello")
+import sys
+import random
 
+people = int(sys.argv[1])
+calendar = int(sys.argv[2])
+iterations = int(sys.argv[3])
+
+sames = 0
+for _ in range(iterations):
+	classroom = []
+	same_birthday = False
+	for i in range(people):
+		birthday = random.randint(0, calendar-1)
+		if birthday in classroom:
+			same_birthday = True
+			break
+		classroom.append(birthday)
+	if same_birthday: sames += 1
+	
+print(sames/iterations)
 
