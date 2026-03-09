62413ce9f0a73ac30b19c091c6d7d47cf2f9ddb9
sydney haubursin <sdhaubursin@ucdavis.edu>
2026-03-08 20:18:39 -0700

    commit

--- 11oligo.py
+++ 11oligo.py
@@ -7,6 +7,6 @@ def calculate_oligi_tm(a, c, g, t):
 		tm = (at_count * 2) + (gc_count * 2)
 	else: 
 		tm = 64.9 + 41 * (gc_count - 16.4) / n
-	return round(tm, 2)
+	return (tm)
 print(calculate_oligi_tm(3, 4, 5, 7))
 print(calculate_oligi_tm(2, 1, 3, 4))
