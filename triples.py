25ee193cdc54d90429fe060496a56b802507ad62
sydney haubursin <sdhaubursin@ucdavis.edu>
2026-03-10 14:05:27 -0700

    midterm

--- triples.py
+++ triples.py
@@ -5,13 +5,13 @@ def main():
 		print("fail")
 		sys.exit(1)
 
-n = int(sys.argv[1])
+	n = int(sys.argv[1])
 
-for a in range(1, n + 1):
-	for b in range(a, n + 1):
-		for c in range(b, n + 1):
-			if a*a + b*b == c*c: 
-				print(a, b, c)
-				
+	for a in range(1, n + 1):
+		for b in range(a, n + 1):
+			for c in range(b, n + 1):
+				if a*a + b*b == c*c: 
+					print(a, b, c)
+					
 if __name__ == "__main__":
 	main()
