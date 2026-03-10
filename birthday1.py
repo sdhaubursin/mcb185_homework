3aae0c334dc80b3584f6efb3962858bcb28c81a6
sydney haubursin <sdhaubursin@ucdavis.edu>
2026-03-10 13:47:47 -0700

    midterm

---
+++ birthday1.py
@@ -0,0 +1,25 @@
+import sys
+import random
+
+def main():
+	if len(sys.argv) != 3:
+		print("fail")
+		sys.exit(1)
+		
+c = int(sys.argv[1]) # calendar size
+n = int(sys.argv[2]) # number of people
+people = []
+
+for i in range(n):
+	birthday = random.randint(1, c)
+	people.append(birthday)
+	
+for i in range(n):
+	for j in range(i + 1, n):
+		if people[i] == people[j]:
+			print("match found")
+		else: 
+			print("match not found")
+			
+if __name__ == "__main__":
+	main()
